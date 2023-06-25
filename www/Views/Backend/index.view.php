5d3adfb15868b29bb034e751040f801a4169604c
AperanoGavin <audesandrine6@gmail.com>
2023-06-25 21:00:33 +0000

    add dashboard

--- www/Views/Backend/index.view.php
+++
@@ -1,4 +0,0 @@
-<h1>test</h1>
-<a href="showuser" class="btn btn-primary">Voir les utilisateurs</a>
-<a href="page/index" class="btn btn-primary">Voir les pages</a>
-<a href="article/index" class="btn btn-primary">Voir les articles</a>
