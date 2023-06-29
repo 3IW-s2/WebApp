e03277b82aefea915194e9f59a11ace5bbf1140c
audesandrine6@gmail.com <audesandrine6@gmail.com>
2023-06-29 08:20:50 +0000

    uploads gestion

--- www/Views/Backend/Image/index.view.php
+++ www/Views/Backend/Image/index.view.php
@@ -1,11 +1,24 @@
 
+
+<form  method="post" enctype="multipart/form-data">  
+    Select File:  
+    <input type="file" name="fileToUpload" accept="image/png, image/gif, image/jpeg"/>  
+    <input type="submit" value="Upload Image" name="submit" />  
+</form>  
 <?php 
- 
+ //afficher toutes les images du dossier uploads avec leur lien
+  $images = scandir("public/uploads/");
+  foreach($images as $image){
+      if($image != "." && $image != ".."){
+        
+        ?>
+
+        <div class="div">
+            <a href="/public/uploads/<?php echo $image ?>"  class="image_paste"><?php echo $image ?></a>
+        </div>
+        <?php
+      }
+  }
  
 
 ?>
-<form  method="post" enctype="multipart/form-data">  
-    Select File:  
-    <input type="file" name="fileToUpload"/>  
-    <input type="submit" value="Upload Image" name="submit"/>  
-</form>  
