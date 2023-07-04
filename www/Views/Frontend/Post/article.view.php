7eda1b873071149a4db6ef059b756f944c655572
audesandrine6@gmail.com <audesandrine6@gmail.com>
2023-07-04 13:19:30 +0000

    article backend  view sound ok

--- www/Views/Frontend/Post/article.view.php
+++ www/Views/Frontend/Post/article.view.php
@@ -1,15 +1,13 @@
 <section id="blogs" class="py-5">
     <div class="container">
-        <div class="title text-center py-5">
-            <h2 class="position-relative d-inline-block"><?= $posts['title'] ;?></h2>
-        </div>
+       <!--  <div class="title text-center py-5">
+        </div> -->
 
         <div class="collection-list mt-5 row gx-0 gy-3">
 
             <div class="col-md-12">
                 <div class="mb-2">
                     <div class="card-body px-0">
-                        <h4 class="card-title"><?= $posts['title'] ;?></h4>
                         <p class="card-text mt-3 text-muted"><?= $posts['content'] ;?></p>
                     </div>
                 </div>
