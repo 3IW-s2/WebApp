09ff7b80e75390259d61fa959517333e41a87a23
audesandrine6@gmail.com <audesandrine6@gmail.com>
2023-07-19 14:28:01 +0000

    review template

--- www/Views/Backend/template.view.php
+++ www/Views/Backend/template.view.php
@@ -1,11 +1,11 @@
 <body>
     <h1>Template</h1>
-    <p>il y'a 2 template de préfait(1 & 2 ) pour en rajouter un  autre il faudra se mettre au html  ;)</p>
+    <p>il y'a <?= $nombreFichiers;?> template de préfait( pour en rajouter un  autre il faudra se mettre au html  ;)</p>
     
 
     <form action="" method="post">
 
-         <input type="number" name="template" id="template">
+         <input type="number" min="0"  max="<?=$nombreFichiers ?>" name="template" id="template">
    
          <button type="submit" name="submit" class="btn btn-primary">Register</button>
 
